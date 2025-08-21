2d9f0ddbedbfc63e4a30e603b22d8f4b8d10bb74
nikitanicvetov <nikita.nitsvetov@gmail.com>
2025-08-21 13:43:57 +0300

    first action

--- test/test_1.py
+++ test/test_1.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.chrome.options import Options
 
 
 import pytest
 
 @pytest.fixture()
 def browser():
-    chrome_browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    chrome_browser = webdriver.Chrome(options=options)
     return chrome_browser
 
 def test_button_exist(browser):
