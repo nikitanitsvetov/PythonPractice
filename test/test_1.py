e135435912f3e5bc47a1df1fd723ad9157193a34
nikitanicvetov <nikita.nitsvetov@gmail.com>
2025-08-21 14:39:50 +0300

    new test

--- test/test_1.py
+++ test/test_1.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-
-
 import pytest
 
 @pytest.fixture()
@@ -10,10 +8,17 @@ def browser():
     options = Options()
     options.add_argument('--headless')
     chrome_browser = webdriver.Chrome(options=options)
+    chrome_browser.implicitly_wait(10)
     return chrome_browser
 
 def test_button_exist(browser):
-    browser.get ('https://www.qa-practice.com/elements/button/simple')
-    assert browser.find_element(By.ID, 'submit-id-submit')
+    browser.get('https://www.qa-practice.com/elements/button/simple')
+    assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
+
+
+def test_button2_exist(browser):
+    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
+    assert browser.find_element(By.CSS_SELECTOR,'.a-button').is_displayed()
+
 
 
